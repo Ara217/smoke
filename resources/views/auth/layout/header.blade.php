062231373e14a902ddf0c8293a52a5eab8b71dff
AraMesropyan <aramesropyan@gmail.com>
2018-08-29 00:53:15 +0400

    Design changer and fixes.

--- resources/views/auth/layout/header.blade.php
+++ resources/views/auth/layout/header.blade.php
@@ -1,19 +1,26 @@
 <nav class="navbar bg-faded  navbar-custom">
     <div class="container">
         <div class="header-logo">
-            <a href="/admin" class="mx-auto">
-                <img src="{{asset('/images/logo-02.png')}}" alt="">
-            </a>
+            @guest
+                <a href="/" class="mx-auto">
+                    <img src="{{asset('/images/logo-02.png')}}" alt="">
+                </a>
+            @else
+                <a href="/admin" class="mx-auto">
+                    <img src="{{asset('/images/logo-02.png')}}" alt="">
+                </a>
+            @endguest
         </div>
-        <ul class="nav navbar-nav navbar-logo mx-auto logo-block">
+        <ul class="logo-block" style="align-self: flex-end;">
+            <h1>Табак Маркет</h1>
             {{--<a href="/admin" class="mx-auto">--}}
-                {{--<img src="{{asset('images/ruseren-logo2.png')}}" class="logo-img" alt="Элит выкуп">--}}
+            {{--<img src="{{asset('images/ruseren-logo2.png')}}" class="logo-img" alt="Элит выкуп">--}}
             {{--</a>--}}
         </ul>
         <ul class="nav navbar-nav pull-sm-right admin-left-navbar">
             @guest
-                <li><a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                {{--<li><a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a></li>--}}
+                <li><a class="nav-link text-white" href="{{ route('login') }}">{{ __('Войти') }}</a></li>
+                <li><a class="nav-link text-white" href="{{ route('register') }}">{{ __('Регистрация') }}</a></li>
             @else
                 <li><a class="nav-link text-white" href="{{route('products.create')}}">Добавить продукт</a></li>
                 <li class="nav-item dropdown">
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,16 +1,49 @@
-<nav class="navbar bg-faded navbar-custom">
-    <div class="container col-md-8">
+{{--<nav class="navbar navbar-custom">--}}
+    {{--<div class="container">--}}
+        {{--<div class="header-logo">--}}
+            {{--<a href="/" class="mx-auto">--}}
+                {{--<img src="{{asset('/images/logo-02.png')}}" alt="">--}}
+            {{--</a>--}}
+        {{--</div>--}}
+        {{--<ul class="logo-block" style="align-self: flex-end;">--}}
+            {{--<h1>Табак Маркет</h1>--}}
+            {{--<a href="/admin" class="mx-auto">--}}
+            {{--<img src="{{asset('images/ruseren-logo2.png')}}" class="logo-img" alt="Элит выкуп">--}}
+            {{--</a>--}}
+        {{--</ul>--}}
+        {{--<ul class="nav navbar-nav navbar-logo mx-auto logo-block">--}}
+            {{--<a href="/" class="mx-auto">--}}
+{{--                <img src="{{asset('images/ruseren-logo2.png')}}" class="logo-img" alt="Элит выкуп">--}}
+            {{--</a>--}}
+        {{--</ul>--}}
+        {{--<ul class="nav navbar-nav pull-sm-right admin-left-navbar">--}}
+            {{--<li>--}}
+                {{--<a href="/products">Список продуктов</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+                {{--<a href="/products/cart" id="cartCount">Корзина</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+                {{--<a href="/about">О нас</a>--}}
+            {{--</li>--}}
+        {{--</ul>--}}
+    {{--</div>--}}
+{{--</nav>--}}
+
+<nav class="navbar bg-faded  navbar-custom">
+    <div class="container">
         <div class="header-logo">
             <a href="/" class="mx-auto">
                 <img src="{{asset('/images/logo-02.png')}}" alt="">
             </a>
         </div>
-        <ul class="nav navbar-nav navbar-logo mx-auto logo-block">
-            <a href="/" class="mx-auto">
-{{--                <img src="{{asset('images/ruseren-logo2.png')}}" class="logo-img" alt="Элит выкуп">--}}
-            </a>
+        <ul class="logo-block" style="align-self: flex-end;">
+            <h1>Табак Маркет</h1>
         </ul>
         <ul class="nav navbar-nav pull-sm-right admin-left-navbar">
+            <li>
+                <a href="{{ route('login') }}">{{ __('Войти') }}</a>
+            </li>
             <li>
                 <a href="/products">Список продуктов</a>
             </li>
