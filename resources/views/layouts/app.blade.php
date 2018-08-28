39a3d20de63ce199d35081a196a3f8e2c6260d7d
AraMesropyan <aramesropyan@gmail.com>
2018-08-29 00:57:11 +0400

    Addded favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-    {{--<link rel="shortcut icon" href="{{{ asset('images/Logo.png') }}}" type="image/x-icon">--}}
+    <link rel="shortcut icon" href="{{{ asset('images/logo-02.png') }}}" type="image/x-icon">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
